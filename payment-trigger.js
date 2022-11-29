857b1a686cbe63e867e27e59f2d4b071fa5396dc
Anton Budantsov <boo6eek@gmail.com>
2022-11-29 14:39:13 +0300

    added payment-trigger.js

--- payment-trigger.js
+++ payment-trigger.js
@@ -2,6 +2,7 @@ const payment = require('./payment');
 const async = require('async');
 const config = require('./config');
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const {mpapi} = require('./js-rpcapi');
 
@@ -12,9 +13,15 @@ mongoose.connect(config.MONGO_URL, {
 }, async (error) => {
   if (error) throw error;
 
+  const lastLevel = parseInt(process.env.LAST_LEVEL)
+
+  if (!_.isNumber(lastLevel)) {
+    throw new Error('LAST_LEVEL is not number')
+  }
+
   await async.eachLimit(config.PAYMENT_SCRIPT.BAKER_PRIVATE_KEYS, 1, async (privateKey) => {
     const bakerKeys = mpapi.crypto.extractKeys(privateKey);
-    await payment.runPaymentScript({bakerKeys, lastLevel: process.env.LAST_LEVEL});
+    await payment.runPaymentScript({bakerKeys, lastLevel});
   });
 
   process.exit(1);
