600671cfebc2e625916ce2725afeadce5b376ca0
Anton Budantsov <boo6eek@gmail.com>
2022-11-29 14:49:44 +0300

    update payment.js

--- payment.js
+++ payment.js
@@ -17,36 +17,34 @@ const runPaymentScript = async ({bakerKeys, lastLevel}) => {
     return;
   }
 
-  const rewardsByAddress = {};
-  const limit = 1000;
-  let countLoadedDocs = 0;
   let limitBreak = false
 
-  while (true) {
-    const rewards = await Reward.find({
-      from: bakerKeys.pkh,
-      level: {$lte: lastLevel},
-      paymentOperationHash: null
-    }).skip(countLoadedDocs).limit(limit);
+  const stream = Reward.find({
+    from: bakerKeys.pkh,
+    level: {$lte: lastLevel},
+    paymentOperationHash: null
+  }).cursor();
 
-    if (!rewards.length) {
-      break;
+  const rewardsByAddress = {};
+  let i = 0;
+  let countLoadedDocs = 0;
+  for (let doc = await stream.next(); doc != null; doc = await stream.next()) {
+    i++;
+    countLoadedDocs++;
+
+    if (i > 1000) {
+      console.log('Loaded docs', countLoadedDocs);
+      i = 0;
     }
 
-    countLoadedDocs += rewards.length;
-
-    console.log('Loaded docs', countLoadedDocs);
-
-    for (const reward of rewards) {
-      if (rewardsByAddress[reward.to]) {
-        rewardsByAddress[reward.to].amountPlexGross += reward.amount;
-        rewardsByAddress[reward.to].rewardIds.push(reward._id);
-      } else {
-        rewardsByAddress[reward.to] = {
-          amountPlexGross: reward.amount,
-          rewardIds: [reward._id]
-        };
-      }
+    if (rewardsByAddress[doc.to]) {
+      rewardsByAddress[doc.to].amountPlexGross += doc.amount;
+      rewardsByAddress[doc.to].rewardIds.push(doc._id);
+    } else {
+      rewardsByAddress[doc.to] = {
+        amountPlexGross: doc.amount,
+        rewardIds: [doc._id]
+      };
     }
 
     if (countLoadedDocs > 5000000) {
