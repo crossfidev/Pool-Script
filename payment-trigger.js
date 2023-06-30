a2ff93cae64640b225918e5a94cbb5c354625521
Anton Budantsov <boo6eek@gmail.com>
2023-06-30 12:44:27 +0300

    update triger

--- payment-trigger.js
+++ payment-trigger.js
@@ -1,10 +1,10 @@
-const payment = require('./payment');
-const async = require('async');
 const config = require('./config');
 const mongoose = require('mongoose');
 const _ = require('lodash');
 
 const {mpapi} = require('./js-rpcapi');
+const lodash = require("lodash");
+const runPayment = require("./payment");
 
 mongoose.connect(config.MONGO_URL, {
   useNewUrlParser: true,
@@ -18,11 +18,10 @@ mongoose.connect(config.MONGO_URL, {
   if (!_.isNumber(lastLevel)) {
     throw new Error('LAST_LEVEL is not number')
   }
-
-  await async.eachLimit(config.PAYMENT_SCRIPT.BAKER_PRIVATE_KEYS, 1, async (privateKey) => {
+  await Promise.all(lodash.map(config.PAYMENT_SCRIPT.BAKER_PRIVATE_KEYS, async (privateKey) => {
     const bakerKeys = mpapi.crypto.extractKeys(privateKey);
-    await payment.runPaymentScript({bakerKeys, lastLevel});
-  });
+    await runPayment({bakerKeys, cycle: lastLevel});
+  }))
 
   process.exit(1);
 })
